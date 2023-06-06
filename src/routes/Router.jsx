93a820f5b9936231b5a9ceca7c9fcc67ed4ecc80
Johurul Haque <johurulhaquejony@gmail.com>
2023-06-06 13:07:47 +0600

    initialized project

---
+++ src/routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1 className="bg-rose-600 text-white">Hello world!</h1>,
+  },
+]);
+
+export default router;
