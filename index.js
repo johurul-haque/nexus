f098cd20fe347ea353b85fd163ce3865932a8848
Johurul Haque <johurulhaquejony@gmail.com>
2023-06-11 01:49:59 +0600

    added get inststructors and classes method, also post method for users

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express'),
   cors = require('cors'),
   { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'),
   app = express(),
-  port = process.env.PORT || 5000;
+  stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -23,12 +24,75 @@ async function run() {
   try {
     client.connect();
 
+    const database = client.db('nexus'),
+      classes = database.collection('classes'),
+      instructors = database.collection('instructors'),
+      users = database.collection('users');
+
     app.get('/', (req, res) => {
       res.send(`<main>
       <h1>Hi, I'm Johurul!</h1>
       <p>An aspiring front-end dev, creating high quality web apps.</p>
       <a href="https://www.linkedin.com/in/johurul-haque/">Linkedin</a>
       </main>`);
+    });
+
+    app.get('/classes', async (req, res) => {
+      if (req.query?.limit) {
+        const cursor = await classes
+          .find({})
+          .limit(parseInt(req.query.limit))
+          .sort({ enrolledStudents: -1 })
+          .toArray();
+        res.send(cursor);
+      } else {
+        const cursor = await classes
+          .find({})
+          .sort({ enrolledStudents: -1 })
+          .toArray();
+        res.send(cursor);
+      }
+    });
+
+    app.get('/instructors', async (req, res) => {
+      if (req.query?.limit) {
+        const cursor = await instructors
+          .find({})
+          .sort({ name: 1 })
+          .limit(parseInt(req.query.limit))
+          .toArray();
+        res.send(cursor);
+      } else {
+        const cursor = await instructors.find({}).sort({ name: 1 }).toArray();
+        res.send(cursor);
+      }
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body,
+        exists = await users.findOne({ email: user.email });
+
+      if (exists) {
+        res.send({ message: 'User already exists' });
+      } else {
+        const result = await users.insertOne(user);
+        res.send(result);
+      }
+    });
+
+    // TODO: Complete integrating payment method
+
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body,
+        amount = price * 100,
+        paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card'],
+        });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     await client.db('admin').command({ ping: 1 });
